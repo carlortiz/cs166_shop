ae37abfb8239053f5f654eec46e0f7aea609e0f7
Carl Luis Ortiz <corti001@xe-10.cs.ucr.edu>
2026-03-02 21:08:58 -0800

    added initiateServiceRequest and closeServiceRequest functions

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         String url = "jdbc:postgresql://localhost:36238/corti001_shop";
         String user = "corti001";
         String password = "";
@@ -15,9 +16,7 @@ public class Main {
             Connection conn = DriverManager.getConnection(url, user, password);
             Scanner scanner = new Scanner(System.in);
         ) {
-
             System.out.println("Connected to database!");
-
             boolean running = true;
 
             while (running) {
@@ -66,4 +65,37 @@ public class Main {
             e.printStackTrace();
         }
     }
+}
+
+// initiate a service request
+public static void initiateServiceRequest(Connection conn, Scanner scanner)
+{
+    System.out.print("Enter last name: ");
+    String lastName = scanner.nextLine();
+
+    String findCustomersByLastName = "SELECT * FROM customer where lastName = ?";
+    PreparedStatement stmt = conn.prepareStatement(sql);
+    stmt.setString(1, lastName);
+    ResultSet rs = stmt.executeQuery();
+
+    // 3. Handle multiple matches
+    if ()
+
+    // 4. Handle no matches
+    // 5. Select or add car
+    // 6. Insert service_request
+    
+}
+
+// close a service request
+public static void closeServiceRequest(Connection conn, Scanner scanner)
+{
+    // TODO:
+    // 1. Ask for service_request_id
+    // 2. Ask for employee_id
+    // 3. Validate request exists
+    // 4. Validate mechanic exists
+    // 5. Validate dates
+    // 6. Update service_request as closed
+    
 }
